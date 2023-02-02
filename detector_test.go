6b3cfdbe0d5514a179189397e0b83bfed45b9564
kpacha <kpacha666@gmail.com>
2023-02-02 20:28:51 +0100

    flag to reject requests with empty user-agent added

--- detector_test.go
+++ detector_test.go
@@ -6,6 +6,28 @@ import (
 	"testing"
 )
 
+func TestNew_rejectEmptyHeader(t *testing.T) {
+	d, err := New(Config{
+		Denylist:  []string{"a", "b"},
+		Allowlist: []string{"c", "Pingdom.com_bot_version_1.1"},
+		Patterns: []string{
+			`(Pingdom.com_bot_version_)(\d+)\.(\d+)`,
+			`(facebookexternalhit)/(\d+)\.(\d+)`,
+		},
+		RejectIfEmpty: true,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	req, _ := http.NewRequest("GET", "http://example.com", nil)
+	req.Header.Add("User-Agent", "")
+	if !d(req) {
+		t.Error("req with empty User-Agent has not been detected as a bot")
+	}
+}
+
 func TestNew_noLRU(t *testing.T) {
 	d, err := New(Config{
 		Denylist:  []string{"a", "b"},
